787111dfd354d91870c2d96de8d3cdd0bb7815d5
Ramon Hentges <r.hentges@hotmail.com>
2022-02-20 22:49:45 -0300

    feat: id on users

--- 11 Flask/usuario.py
+++ 11 Flask/usuario.py
@@ -1,10 +1,13 @@
 class Usuario:
     quantidade = 0
+    id = 0
 
     def __init__(self, nome, email):
+        self.id = Usuario.id
         self.nome = nome
         self.email = email
         Usuario.quantidade += 1
+        Usuario.id += 1
 
     def imprime_usuario(self):
         print(f"{self.nome} ({self.email})")
