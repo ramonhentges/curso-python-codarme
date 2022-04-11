3b0ff8d5dae414ed1a8e00dac9159fdc0284a850
Ramon Hentges <r.hentges@hotmail.com>
2022-04-10 22:27:06 -0300

    test: listagem

--- 14 REST Framework/agenda/tests.py
+++ 14 REST Framework/agenda/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from datetime import datetime, timezone
+import json
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from agenda.models import Agendamento
+from agenda.serializers import AgendamentoSerializer
+
+
+class TestListagemAgendamentos(APITestCase):
+    def test_listagem_vazia(self):
+        response = self.client.get("/api/agendamentos/")
+        data = json.loads(response.content)
+        self.assertEqual(data, [])
+
+    def test_listagem_de_agendamentos_criados(self):
+        Agendamento.objects.create(data_horario=datetime.now(tz=timezone.utc
+                                                             ), nome_cliente="Ramon", email_cliente="cied@ts.co.po", telefone_cliente="+5599986308495")
+        response = self.client.get("/api/agendamentos/")
+        data = json.loads(response.content)
+        self.assertEqual(len(data), 1)
